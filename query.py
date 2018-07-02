77de6c105f639b19a2127695e68077c89f8f9080
Paulina Zheng <paulinaczheng@gmail.com>
2018-07-02 18:03:23 -0400

    Done.

--- query.py
+++ query.py
@@ -1,2 +1,22 @@
 class Query:
-    pass 
+    @classmethod
+    def count(cls, OtherClass):
+        return len(OtherClass._all)
+
+    @classmethod
+    def find_by_name(cls, OtherClass, name):
+        return [item for item in OtherClass._all if item._name == name][0]
+
+    @classmethod
+    def name_starts_with(cls, OtherClass, letter):
+        upper_letter = letter.upper()
+        return [item for item in OtherClass._all if item._name.upper().startswith(upper_letter)]
+
+    @classmethod
+    def is_older_than(cls, OtherClass, age):
+        return [item for item in OtherClass._all if item._age > age]
+
+    @classmethod
+    def mean_age(cls, OtherClass):
+        all_ages = [item._age for item in OtherClass._all]
+        return (sum(all_ages))/(len(OtherClass._all))
