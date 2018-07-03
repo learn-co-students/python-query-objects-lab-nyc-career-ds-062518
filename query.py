f44daf9f890a93ccbd6d829ef41415ff30123138
David Masse <davidjmasse@hotmail.com>
2018-07-03 11:00:24 -0400

    Done.

--- query.py
+++ query.py
@@ -1,2 +1,21 @@
 class Query:
-    pass 
+
+    @classmethod
+    def count(cls, queriedclass):
+        return len(queriedclass.all())
+
+    @classmethod
+    def find_by_name(cls, queriedclass, name):
+        return [pers for pers in queriedclass.all() if pers._name == name][0]
+
+    @classmethod
+    def name_starts_with(cls, queriedclass, firstletter):
+        return [pers for pers in queriedclass.all() if pers._name[0] == firstletter]
+
+    @classmethod
+    def is_older_than(cls, queriedclass, cutoffage):
+        return [pers for pers in queriedclass.all() if pers._age > cutoffage]
+
+    @classmethod
+    def mean_age(cls, queriedclass):
+        return round(sum([pers._age for pers in queriedclass.all()]) / cls.count(queriedclass), 2)
