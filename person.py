783efd11f1c8719b1817d7fa8ed84b7d032ac89f
mattymecks <mcmecoli@gmail.com>
2018-07-03 13:33:59 -0400

    Done.

--- person.py
+++ person.py
@@ -1,4 +1,53 @@
-# import your Query class here
+# import your Query class
+
+from query import Query
 
 class Person:
-    pass
+
+    _all = []
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+        Person._all.append(self)
+
+    @classmethod
+    def count(cls):
+        return Query.count(cls)
+
+    @classmethod
+    def find_by_name(cls, name):
+        return Query.find_by_name(cls, name)
+
+    @classmethod
+    def name_starts_with(cls, first_letter):
+        return Query.name_starts_with(cls, first_letter)
+
+    @classmethod
+    def is_older_than(cls, age):
+        return Query.is_older_than(cls, age)
+
+    @classmethod
+    def mean_age(cls):
+        return Query.mean_age(cls)
+
+    #decorators for _name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+
+    #decorarors for _age
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    #decorators for _age
--- query.py
+++ query.py
@@ -1,2 +1,36 @@
 class Query:
-    pass 
+
+    @classmethod
+    def count(cls, otherClass):
+        count = 0
+        for item in otherClass._all:
+            count += 1
+        return count
+
+    @classmethod
+    def find_by_name(cls, otherClass, name):
+        for item in otherClass._all:
+            if item._name == name:
+                return item
+
+    @classmethod
+    def name_starts_with(cls, otherClass, first_letter):
+        name_starts_with = []
+        for item in otherClass._all:
+            if item._name.startswith(first_letter):
+                name_starts_with.append(item)
+        return name_starts_with
+
+    @classmethod
+    def is_older_than(cls, otherClass, age):
+        oldies = []
+        for item in otherClass._all:
+            if item._age > age:
+                oldies.append(item)
+        return oldies
+
+    @classmethod
+    def mean_age(cls, otherClass):
+        all_ages = [item._age for item in otherClass._all]
+        mean_age = sum(all_ages)/len(all_ages)
+        return mean_age
